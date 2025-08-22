7b9ab62f03252e8262a51d8625cd1e2a695eed21
Praveen Sunhare <praveen786sunhare@gmail.com>
2025-08-22 21:38:42 +0530

    Fix validation from backend api's

--- src/controller/trips-controller.js
+++ src/controller/trips-controller.js
@@ -4,17 +4,19 @@ const Trips = require("../models/Trips");
 const createTrip = async (req, res) => {
   try {
     const { name } = req.body;
-    const tripCreatedRes = await Trips.create({ name });
-    res.status(201).json(tripCreatedRes);
+    if (!name) res.status(401).json({ message: 'Trip name is not provided.', status: 0 })
+    await Trips.create({ name });
+    res.status(201).json({ message: 'Trip is created successfully', status: 1 });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message, status: 0 });
   }
 };
 
 // Get all trips
 const getAllTrips = async (req, res) => {
   try {
-    const trips= await Trips.find();
+    const trips = await Trips.find().select({ name: 1, _id: 0, createdAt: 0 });
+    if (!trips.length) res.status(401).json({ message: 'No trips found', status: 0 })
     res.json(trips);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- src/routes/trips-route.js
+++ src/routes/trips-route.js
@@ -3,7 +3,7 @@ const { createTrip, getAllTrips } = require("../controller/trips-controller");
 
 const router = express.Router();
 
-router.post("/add-trip", createTrip);  
-router.get("/get-trips", getAllTrips);     
+router.post("/add-trip", createTrip);
+router.get("/get-trips", getAllTrips);
 
 module.exports = router;
